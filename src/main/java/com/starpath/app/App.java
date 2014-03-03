3508a5b264be134f0cd4eef130ee80ea9ce48812
Kumar Kothapalli <P@ssword2012>
2014-03-02 19:36:41 -0700

    Expecting not to commit .ipr

---
+++ src/main/java/com/starpath/app/App.java
@@ -0,0 +1,13 @@
+package com.starpath.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
